57eb5cdc67c4e6d793248bd0e5bf8963247fa88a
julianbarbieri1 <julian.barbieri@nybblegroup.com>
2022-10-04 15:48:25 -0300

    Cambios Juli

---
+++ src/main/java/desarrollo/Bug.java
@@ -0,0 +1,19 @@
+package desarrollo;
+
+public class Bug extends Issues{
+
+    private String expectedResult;
+
+
+    public Bug() {
+        this.expectedResult = expectedResult;
+    }
+
+    public String getExpectedResult() {
+        return expectedResult;
+    }
+
+    public void setExpectedResult(String expectedResult) {
+        this.expectedResult = expectedResult;
+    }
+}
---
+++ src/main/java/desarrollo/Issues.java
@@ -0,0 +1,60 @@
+package desarrollo;
+
+public abstract class Issues {
+
+    private String titulo;
+    private String descripcion;
+    private String prioridad;
+    private Pasos pasos;
+    private String severidad;
+    private String actualResult;
+
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getPrioridad() {
+        return prioridad;
+    }
+
+    public void setPrioridad(String prioridad) {
+        this.prioridad = prioridad;
+    }
+
+    public Pasos getPasos() {
+        return pasos;
+    }
+
+    public void setPasos(Pasos pasos) {
+        this.pasos = pasos;
+    }
+
+    public String getSeveridad() {
+        return severidad;
+    }
+
+    public void setSeveridad(String severidad) {
+        this.severidad = severidad;
+    }
+
+    public String getActualResult() {
+        return actualResult;
+    }
+
+    public void setActualResult(String actualResult) {
+        this.actualResult = actualResult;
+    }
+}
