4ecf74eae5560f3855f9f3249d475096b03c7a90
Enzo Lagos <enzo.lagos@nybblegroup.com>
2022-10-04 15:13:54 -0300

    Agrego clase TestCase

---
+++ src/main/java/TestCase.java
@@ -0,0 +1,63 @@
+public class TestCase {
+    private String title;
+    private String priority;
+    private String description;
+    private Pasos pasos;
+    private String preconditions;
+    private String status;
+
+    public TestCase(String title, String priority, Pasos pasos, String status) {
+        this.title = title;
+        this.priority = priority;
+        this.pasos = pasos;
+        this.status = status;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Pasos getPasos() {
+        return pasos;
+    }
+
+    public void setPasos(Pasos pasos) {
+        this.pasos = pasos;
+    }
+
+    public String getPreconditions() {
+        return preconditions;
+    }
+
+    public void setPreconditions(String preconditions) {
+        this.preconditions = preconditions;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
